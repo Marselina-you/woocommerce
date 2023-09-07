dadefecd2472f4dea70c7c1f67e62c8bfa6ab889
Marselina-you <marselina88@list.ru>
2023-09-07 07:27:47 +0300

    video 4.1 logotip

--- wp-content/themes/burrito/header.php
+++ wp-content/themes/burrito/header.php
@@ -26,6 +26,40 @@
 <div id="page" class="site">
 
 	<header id="masthead" class="site-header header">
+	<?php $logo_id = carbon_get_theme_option('est_header_logo'); 
+	$logo = $logo_id ? wp_get_attachment_image_src($logo_id , 'full') : '';
+	//get_vd($logo);?>
+
+
+	<?php if (is_front_page() && is_home()) :
+		if ($logo_id) :	?>
+		<h1>
+			<a href="<?php echo home_url('/');?>"> <img src="<?php echo $logo[0];?>" width="<?php echo $logo[1];?>" height="<?php echo $logo[2];?>" alt="">
+			</a>
+		</h1>
+			<?php else: ?>
+			<h1>
+				<a href="<?php echo home_url('/');?>">
+					<?php echo $site_name; ?><span><?php echo $site_decs; ?></span>
+				</a>
+			</h1>
+		<?php endif;?>
+	<?php else:
+		if ($logo_id) :	?>
+		<h1>
+			<img src="<?php echo $logo[0];?>" width="<?php echo $logo[1];?>" height="<?php echo $logo[2];?>" alt="">
+			
+		</h1>
+	<?php else: ?>
+		<h1>
+	
+				<?php echo $site_name; ?><span><?php echo $site_decs; ?></span>
+
+		</h1>
+	<?php endif;?>
+	<?php endif;?>
+
+	?>
 		<?php
 		/**
 		 * header_parts hook.
@@ -42,6 +76,7 @@
 		 */
 		do_action('header_parts');
 		?>
+		
 
 	</header><!-- #masthead -->
 
